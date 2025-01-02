e9dece784588ff7da7a7171ece281b1a55174f49
Paula Airaudo <paula.airaudo@hotmail.com>
2025-01-01 21:43:22 -0300

    feat: enhance CardProject component with image slider and project details

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,35 +1,74 @@
+import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import Typography from '@mui/material/Typography';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 interface CardProjectProps {
   name: string;
   description: string;
-  image_url: string;
+  built: string;
+  image_url: string[];
   code_url: string;
   visit_url: string;
 }
 
-function CardProject({ name, description, code_url, visit_url, image_url}: CardProjectProps) {
+function CardProject({ name, description, built, code_url, visit_url, image_url}: CardProjectProps) {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    appendDots: (dots: React.ReactNode) => (
+      <div
+        style={{
+          backgroundColor: 'transparent',
+          borderRadius: '1rem',
+          bottom: '-2rem',
+        }}
+      >
+        <ul style={{ margin: '0px' }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: (i: number) => (
+      <div
+        style={{
+          width: '0.4rem',
+          height: '0.4rem',
+          backgroundColor: '#bdbdbd',
+          borderRadius: '50%',
+        }}
+      />
+    ),
+  };
 
   return (
-    <Card sx={{ minWidth: '80vw', margin: '2rem', background: 'rgba(1, 1, 1,0.3)' }}>
-      <CardMedia
-        component="img"
-        alt={name}
-        height="250"
-        image={image_url}
-      />
-      <CardContent>
+    <Card sx={{minWidth: '60vw', margin: '2rem', background: 'rgba(1, 1, 1,0.10)', overflow: 'visible'}}>
+      <Slider {...settings}>
+        {image_url.map((image_url, index) => (
+          <div key={index}>
+            <CardMedia component="img" alt={name} image={image_url} sx={{scale: '95%', borderRadius:'0.5rem'}} />
+          </div>
+        ))}
+      </Slider>
+      <CardContent sx={{padding: "2rem"}}>
         <Typography gutterBottom variant="h3" component="div">
           {name}
         </Typography>
-        <Typography variant="body1" color="text.secondary">
+        <Typography variant='body1' color="text.primary">
+          {built}
+        </Typography>
+        <Typography variant='body2' color="text.secondary" marginTop='1rem'>
           {description}
         </Typography>
       </CardContent>
@@ -37,6 +76,7 @@ function CardProject({ name, description, code_url, visit_url, image_url}: CardP
         <Button href={code_url} target="_blank" size="large" sx={{fontFamily: 'monospace', fontWeight: '800'}}><GitHubIcon sx={{margin:'0.2em'}}/>CODE</Button>
         <Button href={visit_url} target="_blank" size="large" sx={{fontFamily: 'monospace', fontWeight: '800'}}><ComputerOutlinedIcon sx={{margin:'0.2em'}}/>VISIT</Button>
       </CardActions>
+      
     </Card>
   );
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,36 +1,48 @@
 import { Box } from '@mui/material';
 import CardProject from '../components/CardProject';
+import LYKMob from '../assets/images/lyk-brewing-co_cap2.png';
+import LYKWeb from '../assets/images/lyk-brewing-co_cap1.png';
+import LYKWeb2 from '../assets/images/lyk-brewing-co_cap1.2.png'
+import HGMob from '../assets/images/hangman_cap2.png';
+import HGWeb from '../assets/images/hangman_cap1.png';
+import HGWeb2 from '../assets/images/hangman_cap1.2.png';
+import PFMob from '../assets/images/portfolio_cap2.png';
+import PFWeb from '../assets/images/portfolio_cap1.png';
+import PFWeb2 from '../assets/images/portfolio_cap1.2.png';
 
 function Projects() {
 
     const projects = [
         {
             name: 'LYK BREWING CO',
-            description: 'React, JavaScript & CSS',
-            image: '',
+            description: 'Explore my virtual brewery store project, showcasing simple search, filtering, shopping cart, page navigation, and form validation functionalities.',
+            built: 'Built with React, JavaScript, and Tailwind CSS.',
+            image: [LYKMob, LYKWeb, LYKWeb2],
             code: 'https://github.com/pauair/lyk-brewing-co',
-            visit: 'https://pauair.github.io/',
+            visit: 'https://pauair.github.io/lyk-brewing-co/',
         },
         {
             name: 'HANGMAN GAME',
-            description: 'React, JavaScript & CSS',
-            image: '',
+            description: 'Check out my hangman game project! I developed challenging gameplay logic, including random word selection, failed attempts management, and visual game progress representation. This project allowed me to enhance my skills in DOM manipulation, event handling, and state management in React.',
+            built: 'Built with React, JavaScript, and Tailwind CSS.',
+            image: [HGMob, HGWeb, HGWeb2],
             code: 'https://github.com/pauair/hangman-game',
             visit: 'https://pauair.github.io/hangman-game/',
         },
         {
-            name: 'ANOTHER PROJECT ',
-            description: 'React, JavaScript & CSS',
-            image: '',
-            code: '',
-            visit: '',
+            name: 'PORTFOLIO ',
+            description: 'Explore my personal portfolio website—your current destination! This concise, one-page showcase of my work is crafted with a focus on user experience design and seamless navigation. It allows visitors to view my projects, learn more about me, and easily get in touch.',
+            built: 'Built with React, TypeScript, Material UI & CSS',
+            image: [PFMob, PFWeb, PFWeb2],
+            code: 'https://github.com/pauair/portfolio',
+            visit: 'https://pauair.github.io/portfolio/',
         }
     ]
 
     return (
-        <Box id="projects" minWidth={'80vw'} sx={{ margin: '10vw', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+        <Box id="projects" minWidth={'80vw'} sx={{ margin: '8vw', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
             {projects.map((project, index) => (
-                <CardProject key={index} name={project.name} description={project.description} image_url={project.image} code_url={project.code} visit_url={project.visit}/>
+                <CardProject key={index} name={project.name} description={project.description} built={project.built} image_url={project.image} code_url={project.code} visit_url={project.visit}/>
             ))}
         </Box>
     );
